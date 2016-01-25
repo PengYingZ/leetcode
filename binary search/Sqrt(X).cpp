841c7286d5f1cc642bb1d0adc7381062e3ece3cf
Peng <18814127731@163.com>
2016-01-25 22:43:57 +0800

    add Sqrt(X).cpp

---
+++ binary search/Sqrt(X).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int left = 0;
+        int right = x;
+        int middle;
+        
+        while((right-left) > 1){
+            middle = left + (right - left) * 0.5;
+            
+            if(x/middle == middle){
+                //即middle的平方刚好等于x,返回middle
+                return middle;
+            }
+            else if(x/middle < middle){
+                right = middle;
+            }
+            else{
+                left = middle;
+            }
+        }
+        return (x<=1)?x:left;
+    }
+};
